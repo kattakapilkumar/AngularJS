75e127d73e722a120de0b55a57bc8c7bf1db1580
kattakapilkumar <kapilkatta123@gmail.com>
2018-03-26 12:02:35 +0530

    AngularJS

--- 2.Module_and_Controllers/Mod_Cont_Example1.js
+++ 2.Module_and_Controllers/Mod_Cont_Example1.js
@@ -1,7 +1,14 @@
 var kapil_model_var = angular.module("kapil_module",[]);
 
 
+
+
+
+
+
+
+
 kapil_model_var.controller("kapil_controller", function($scope)
 {
-   $scope.message = "Angular tutorials";
+   $scope.message="Angular tutorials";
 });
---
+++ 2.Module_and_Controllers/Mod_Cont_Example2_Wrng_Con_Name.js
@@ -0,0 +1,13 @@
+var kapil_model_var1 = angular.module("kapil_module_e",[]);
+
+
+kapil_model_var1.controller("kapil_controller_e_wrong", function($scope)
+{
+    var employee1_k =
+    {
+         firstName : "kapil",
+         lastName  : "kumar",
+         gender    : "male"
+    };
+   $scope.scope_employee = employee1_k;
+});
